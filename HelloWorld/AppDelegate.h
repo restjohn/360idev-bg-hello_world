cb5dc506904b387cf346de13e542eaef09c9a7bb
Robert St. John <stjohnr@bit-sys.com>
2014-08-24 13:55:27 -0600

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Robert St. John on 8/24/14.
+//  Copyright (c) 2014 BIT Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
